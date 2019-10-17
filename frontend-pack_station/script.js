9190bee6e0f2cf77d31616bd432be117c0fa63a0
Vjadro <darosoro@gmail.com>
2019-10-17 17:56:34 +0200

    added some product styles

--- frontend-pack_station/script.js
+++ frontend-pack_station/script.js
@@ -1,3 +1,16 @@
+let sample_order = {
+	'order_id': '154658974514',
+	'products': [
+		{
+			'ppc': '514569875412354569',
+			'name': 'Kalosze',
+			'type': 'Buty'
+		}
+	]
+};
+
 $(document).ready(function(){
 	console.log('doc ready');
+	$(".order-id").html(sample_order.order_id);
+	$(".products-total").html(sample_order.products.length);
 });
